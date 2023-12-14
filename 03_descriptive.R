19f38595a924f6a8e3abfaa20466dea41f1254d1
benhuynh <benhuynh3@gmail.com>
2023-12-13 23:18:03 -0500

    setting up for propensity model ML code

--- 03_descriptive.R
+++ 03_descriptive.R
@@ -40,7 +40,27 @@ sum(testedDF$`5 Minute` >= 15)/nrow(testedDF)
 
 
 #tests representativeness of data
+allBlocksDF <- imputeDF %>% distinct(blockNum, .keep_all=T)
 
 untestedDemographics <- getRaceEstimates(untestedDF)
 testedDemographics <- getRaceEstimates(testedDF)
-  
+#allBlocksDemographics <- getRaceEstimates(allBlocksDF) this isn't correct since not population weighted
+untestedDemographicsUI <- getRaceEstimatesUI(untestedDemographics)
+testedDemographicsUI <- getRaceEstimatesUI(testedDemographics)
+#allBlocksDemographicsUI <- getRaceEstimatesUI(allBlocksDemographics)  
+
+
+write_csv(as.data.frame(untestedDemographicsUI),"data/processed/untestedDemographics.csv")
+write_csv(as.data.frame(testedDemographicsUI),"data/processed/testedDemographics.csv")
+write_csv(as.data.frame(allBlocksDemographicsUI),"data/processed/allBlocksDemographics.csv")
+
+#percentage of tests and blocks with lead > 1ppb
+
+nrow(testedDF %>% filter(overOne_2))/nrow(testedDF)
+
+blockLeadDF <- testedDF %>% 
+  mutate(blockNum==factor(blockNum)) %>% 
+  group_by(blockNum) %>% 
+  summarize(blockLead = mean(as.numeric(overOne_2))) %>% 
+  mutate(blockClass = ifelse(blockLead>=.5,T,F))
+
